978259fafd59aa052a17be69495e838342f9871d
elmir404 <elmir.gasimzada@gmail.com>
2023-11-25 00:15:33 +0400

    first commit

---
+++ Letter.WebUI/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using Letter.WebUI.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace Letter.WebUI.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+		public IActionResult Contact()
+		{
+			return View();
+		}
+		public IActionResult About()
+		{
+			return View();
+		}
+		public IActionResult Pricing()
+		{
+			return View();
+		}
+		[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Letter.WebUI/Controllers/MessageController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Letter.WebUI.Controllers
+{
+	public class MessageController : Controller
+	{
+		public IActionResult Create()
+		{
+			return View();
+		}
+		public IActionResult Preview()
+		{
+			return View();
+		}
+	}
+}
